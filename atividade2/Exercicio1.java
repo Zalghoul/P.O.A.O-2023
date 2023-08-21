9c77d09c0a02d98d8190357012b38661873f30ca
Hudson C <119542633+Zalghoul@users.noreply.github.com>
2023-08-21 18:33:47 -0300

    Create Exercicio1.java

---
+++ atividade2/Exercicio1.java
@@ -0,0 +1,46 @@
+package calculadora;
+import java.util.Scanner;
+
+public class Calculadora {
+	public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o primeiro número: ");
+        double num1 = scanner.nextDouble();
+
+        System.out.print("Digite o segundo número: ");
+        double num2 = scanner.nextDouble();
+
+        System.out.print("Digite a operação (+, -, *, /): ");
+        char operator = scanner.next().charAt(0);
+
+        double result = 0;
+
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Erro: Divisão por zero não é permitida.");
+                    return;
+                }
+                break;
+            default:
+                System.out.println("Operação inválida.");
+                return;
+        }
+
+        System.out.println("Resultado: " + result);
+
+        scanner.close();
+    }
+}
